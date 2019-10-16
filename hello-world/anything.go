94d9825fc3289f55cc2c30251885ea15e324089b
Samuel Walker <sfxworks@gmail.com>
2019-10-16 01:37:47 -0400

    hello world

---
+++ hello-world/anything.go
@@ -0,0 +1,36 @@
+package main //gotta has
+
+import "fmt"
+
+func main() { //We main gets called automatigically like as3 things
+	fmt.Println("oh hi I really want to learn go so I can do web and k8s things wee")
+
+	foo()
+
+	fmt.Println("Oh cool  it autofills a lot")
+
+	for i := 0; i < 100; i++ { // ooo go fmt made things formatted
+		if i%2 == 0 { //Modulus 2 so only print numbers ooo
+			fmt.Println(i) //Oh man I tried to type in () with the if statement a thousand times in my head
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("Another new language with functions weeeee")
+}
+
+func bar() {
+	fmt.Println("this guy is writing a song with this tutorial oooo haha")
+	fmt.Println("udemy.com/course/learn-how-to-code/learn/lecture/11922028#overview")
+}
+
+//control flow
+// (1) sequential
+// (2) loop; iterative
+// (3) conditional
+// I know this process but didnt know it had a name cool
+
+//Cant have unused vars on code woah awesome.
